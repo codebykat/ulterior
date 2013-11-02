d2bc6dcfd489fdf9957078f69dd3d2ec5160123c
Kat Hagan <kat@codebykat.com>
2013-11-02 02:21:51 -0700

    I'm feeling lucky!

--- ulterior/views.py
+++ ulterior/views.py
@@ -5,23 +5,37 @@ from flask import render_template, request
 
 from random import randint, choice
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def madlib():
+	rand = randint(1, Madlib.query.count())	
+	m = Madlib.query.get(rand)
+
+	return render_template('madlib.html', madlib=m)
+
+
+@app.route('/motive', methods=['GET', 'POST'])
 def motive():
+	rand = randint(1, Prefix.query.count())
+	prefix = Prefix.query.get(rand)
 
-	if(request.method == 'POST'):
-		rand = randint(1, Prefix.query.count())
-		prefix = Prefix.query.get(rand)
-
+	if(request.method == 'GET'):
+		rand = randint(1, Madlib.query.count())	
+		m = Madlib.query.get(rand)
+	else:
 		m = Madlib.query.get(request.form['madlib_id'])
 
-		sentence = m.sentence
+	sentence = m.sentence
 
-		blanks = m.blanks
-		i=0
+	# i=0
 
-		for blank in m.blanks:
-		# for word in request.form.getlist("madlib_blank[]"):
-			submitted_val = request.form["madlib-blank-"+str(blank.word_type.id)]
+	for blank in m.blanks:
+		if(request.method == 'GET'):
+			if(blank.word_type.words.all() == []):
+				word = "(nothing found)"
+			else:
+				word = choice(blank.word_type.words.all()).text
+		else:
+			word = request.form["madlib-blank-"+str(blank.word_type.id)]
 			# TODO add new words to the dictionary
 			# wt = blanks[i]
 			# i += 1
@@ -30,22 +44,11 @@ def motive():
 			# word_in_dictionary = Word.query.filter(Word.word_type == wt).filter(Word.text == word)
 			# if word_in_dictionary is None:
 			# 	Word(word_type, word)
-				# TODO save to database
+			# TODO save to database
 
-			sentence = sentence.replace('{}', submitted_val, 1)
-		# for blank in m.blanks:
-		# 	if(blank.words.all() == []):
-		# 		word = "(nothing found)"
-		# 	else:
-		# 		word = choice(blank.words.all()).text
-		# 	sentence = sentence.replace('{}', word, 1)
+		sentence = sentence.replace('{}', word, 1)
 
-		# TODO save generated sentence for posterity - maybe only on up-vote?
 
-		return render_template('motive.html', prefix=prefix, motivation=sentence)
+	# TODO save generated sentence for posterity - maybe only on up-vote?
 
-	# implicit else - GET request displays a madlib form
-	rand = randint(1, Madlib.query.count())	
-	m = Madlib.query.get(rand)
-
-	return render_template('madlib.html', madlib=m)
+	return render_template('motive.html', prefix=prefix, motivation=sentence)
