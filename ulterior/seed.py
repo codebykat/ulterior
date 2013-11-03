42af35325fb599d67a862efccb4f1d60d336d103
Kat Hagan <kat@codebykat.com>
2013-11-02 22:08:26 -0700

    more patterns, fixing up words

--- ulterior/seed.py
+++ ulterior/seed.py
@@ -3,13 +3,16 @@ from ulterior.models import Prefix, Word, Tag, Madlib
 
 
 prefixes = [
-	"Your true passion is ",
-	"Your highest goal is ",
-	"Your purpose in life is ",
-	"The deepest desire of your heart is ",
-	"The passion that drives you is the need ",
-	"Secretly, you really want ",
-	"You are on a quest ",
+	"Your true passion is to ",
+	"Your highest goal is to ",
+	"Your purpose in life is to ",
+	"The deepest desire of your heart is to ",
+	"The passion that drives you is the need to ",
+	"Secretly, you really want to ",
+	"You are on a quest to ",
+	"More than anything, you want to ",
+	"Most of all, you want to",
+	"You will not rest until you ",
 ]
 
 tags = [
@@ -22,22 +25,23 @@ tags = [
 	'food',
 	'noun',
 	'number',
+	'past-tense verb',
 	'person',
 	'place',
 	'plural noun',
-	'past-tense verb',
+	'profession',
 	'relative',
 	'vehicle',
 	'verb',
 ]
 
 words = {
-	'bees':								[ 'plural noun', 'animal', ],
+	'bees':								[ 'plural noun' ],
 	'bicycle':							[ 'noun', 'vehicle', ],
 	'candies':							[ 'plural noun', 'food', ],
 	'car':								[ 'noun', 'vehicle', ],
 	'cat':								[ 'noun', 'animal', ],
-	'cats':								[ 'plural noun', 'animal', ],
+	'cats':								[ 'plural noun', ],
 	'demon':							[ 'noun', 'animal', ],
 	'dog':								[ 'noun', 'animal', ],
 	'ears':								[ 'plural noun', 'body part', ],
@@ -67,10 +71,16 @@ words = {
 	'murdered':		[ 'past-tense verb', ],
 	'raised':		[ 'past-tense verb', ],
 	'rescue':		[ 'verb', ],
+	'seduce':		[ 'verb', ],
 
 	'colorless':	[ 'adjective', ],
 	'happy':		[ 'adjective', ],
 	'ugly':			[ 'adjective', ],
+	'powerful':		[ 'adjective', ],
+	'popular':		[ 'adjective', ],
+	'famous':		[ 'adjective', ],
+	'rich':			[ 'adjective', ],
+	'mad':			[ 'adjective', ],
 
 	'furiously':	[ 'adverb', ],
 
@@ -89,24 +99,38 @@ words = {
 	'42':			[ 'number', ],
 	'one million':	[ 'number', ],
 
-	'mother':		[ 'relative', ],
-	'father':		[ 'relative', ],
-	'brother':		[ 'relative', ],
-	'sister':		[ 'relative', ],
+	'your mother':		[ 'relative', 'person', ],
+	'your father':		[ 'relative', 'person', ],
+	'your brother':		[ 'relative', 'person', ],
+	'your sister':		[ 'relative', 'person', ],
 
 	'love':			[ 'emotion', ],
 	'hate':			[ 'emotion', ],
+	'happiness':	[ 'emotion', ],
+	'sadness':		[ 'emotion', ],
+
+	'doctor':				[ 'profession', ],
+	'computer programmer':	[ 'profession', ],
+	'fireman':				[ 'profession', ],
+	'scientist':			[ 'profession', ],
 }
 
 
 madlibs = [
-	"to take over {{place}}",
-	"to {{verb}} your {{relative}}, who was {{past-tense verb}} by a {{animal}}",
-	"to find true {{emotion}}",
-	"to acquire {{place}} for strategic {{animal}} purposes",
-	"to collect all the {{plural noun}} in {{place}} and stick them in your {{body part}}",
-	"to steal from the {{adjective}} and give to the {{adjective}}",
-	"to save up {{number}} {{plural noun}} so you can buy a really {{adjective}} {{noun}}",
+	"take over {{place}}",
+	"{{verb}} {{person}}, who was {{past-tense verb}} by a {{animal}}",
+	"find true {{emotion}}",
+	"acquire {{place}} for strategic {{animal}} purposes",
+	"collect all the {{plural noun}} in {{place}} and stick them in your {{body part}}",
+	"steal from the {{adjective}} and give to the {{adjective}}",
+	"save up {{number}} {{plural noun}} so you can buy a really {{adjective}} {{noun}}",
+	"become {{adjective}} and {{adjective}}",
+	"win the {{emotion}} of a {{adjective}} {{profession}}",
+	"become the most {{adjective}} {{profession}} in {{place}}",
+	"be more {{adjective}} than {{person}}",
+	"learn the answer to the ultimate question of {{noun}}, {{place}} and {{noun}}",
+	"find a cure for {{relative}}, who has come down with a {{adjective}} illness",
+	"{{verb}} the {{adjective}} {{profession}} who {{past-tense verb}} {{person}}",
 ]
 
 
